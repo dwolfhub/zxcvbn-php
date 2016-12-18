4e81f18f11487ef9cf7e1848f67956ddb82a157c
Daniel Wolf <danielrwolf5@gmail.com>
2016-12-18 10:00:50 -0600

    date and regex estimator class logic

--- src/Guess/DateEstimator.php
+++ src/Guess/DateEstimator.php
@@ -1,7 +1,25 @@
 <?php
 namespace ZxcvbnPhp\Guess;
 
+use ZxcvbnPhp\Scoring;
+
+/**
+ * Class DateEstimator
+ * @package ZxcvbnPhp\Guess
+ */
 class DateEstimator extends AbstractEstimator
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function estimate()
+    {
+        $yearSpace = max(abs($this->match['year'] - Scoring::REFERENCE_YEAR), Scoring::MIN_YEAR_SPACE);
+        $guesses = $yearSpace * 365;
+        if (!empty($match['separator'])) {
+            $guesses *= 4;
+        }
 
+        return $guesses;
+    }
 }
--- src/Guess/RegexEstimator.php
+++ src/Guess/RegexEstimator.php
@@ -1,7 +1,38 @@
 <?php
 namespace ZxcvbnPhp\Guess;
 
+use ZxcvbnPhp\Scoring;
+
+/**
+ * Class RegexEstimator
+ * @package ZxcvbnPhp\Guess
+ */
 class RegexEstimator extends AbstractEstimator
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function estimate()
+    {
+        $charClassBases = [
+            'alpha_lower' => 26,
+            'alpha_upper' => 26,
+            'alpha' => 52,
+            'alphanumeric' => 62,
+            'digits' => 10,
+            'symbols' => 33,
+        ];
 
+        if (array_key_exists($this->match['regex_name'], $charClassBases)) {
+            return $charClassBases[$this->match['regex_name']] ** strlen($this->match['token']);
+        } else if ($this->match['regex_name'] === 'recent_year') {
+            // conservative estimate of year space: num years from REFERENCE_YEAR.
+            // if year is close to REFERENCE_YEAR, estimate a year space of
+            // MIN_YEAR_SPACE.
+            $yearSpace = abs((int)$this->match['regex_match'][0] - Scoring::REFERENCE_YEAR);
+            $yearSpace = max($yearSpace, Scoring::MIN_YEAR_SPACE);
+
+            return $yearSpace;
+        }
+    }
 }
