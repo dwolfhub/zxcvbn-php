4f959805d3aa8561f276e40e7959a925a7dc4616
Daniel Wolf <danielrwolf5@gmail.com>
2017-01-25 14:15:27 -0600

    began l33t match tests

--- test/unit/Match/L33tMatchTest.php
+++ test/unit/Match/L33tMatchTest.php
@@ -2,6 +2,7 @@
 
 namespace unit\Match;
 
+use Zxcvbn\Match\DictionaryMatch;
 use Zxcvbn\Match\L33tMatch;
 
 class L33tMatchTest extends \PHPUnit_Framework_TestCase
@@ -13,11 +14,27 @@ class L33tMatchTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
+        $mockDictionaryMatch = $this->getMockBuilder(
+            DictionaryMatch::class
+        )
+            ->setMethods(['getMatches'])
+            ->getmock();
+        $mockDictionaryMatch->expects($this->once())
+            ->method('getMatches')
+            ->willReturn([]);
+
         $this->l33tMatch = new L33tMatch();
+        $this->l33tMatch->setDictionaryMatch(
+            $mockDictionaryMatch
+        );
+        $this->l33tMatch->setL33tTable(
+            L33tMatch::DEFAULT_L33T_TABLE
+        );
     }
 
-    public function test()
+    public function testIsTesting()
     {
-
+        $this->l33tMatch->setPassword('password');
+        $this->assertEquals([], $this->l33tMatch->getMatches());
     }
 }
